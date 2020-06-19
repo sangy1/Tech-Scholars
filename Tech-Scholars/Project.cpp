2b746ebde43425aa8b776b3d84d53b0504ada259
iamege <65535047+iamege@users.noreply.github.com>
2020-06-18 21:40:40 -0400

    Update Project.cpp

--- Tech-Scholars/Project.cpp
+++ Tech-Scholars/Project.cpp
@@ -1,8 +1,60 @@
 #include <iostream>
-using namespace std;
+#include <vector>
+#include <fstream>
+#include <string>
+#include <iterator>
+#include <sstream>
+#include <map>
+#include <algorithm>
+#include <locale>
 
-int main() {
-  cout << "Dorjee was here";
-  cout << "ege was here";
-  return 0;
+void insert(std::map<std::string, int> &map1, std::string word)
+{
+     if (map1.find(word) != map1.end())
+          map1[word]++;
+     else          map1.insert(std::map<std::string,int>::value_type(word,1));
+}
+
+typedef std::pair<std::string,int> pair;
+
+int main () {
+     std::string line;
+     std::ifstream myfile ("/Users/egekacmaz/Downloads/jobs.csv");
+     if (myfile.is_open())
+     {
+          std::map<std::string, int> myMap;
+          while ( getline (myfile, line, ',') )
+          {
+               if (!(line.empty()) || !(line.find_first_not_of(' ') == std::string::npos))
+               {
+                    std::stringstream ss(line);
+                    std::istream_iterator<std::string> begin(ss);
+                    std::istream_iterator<std::string> end;
+                    std::vector<std::string> vstrings(begin, end);
+                    for(std::vector<std::string>::const_iterator i = vstrings.begin(); i != vstrings.end(); ++i)
+                    {
+                         std::string check_word = *i;
+                         check_word.erase(std::remove_if(check_word.begin(), check_word.end(), [](char c) { return !std::isalnum(c); }), check_word.end());
+                         std::for_each(check_word.begin(), check_word.end(), [](char & c) {c = ::tolower(c);});
+                         insert(myMap, check_word);
+                    }
+               }
+          }
+
+          std::vector<pair> vec;
+         std::copy(myMap.begin(), myMap.end(),std::back_inserter<std::vector<pair>>(vec));
+         std::sort(vec.begin(), vec.end(), [](const pair& l, const pair& r) {
+            if (l.second != r.second)
+                return l.second > r.second;
+            return l.first > r.first;
+        });
+
+         for (auto const &pair: vec) {
+             std::cout << "Word: " << pair.first << "\tCount: " << pair.second << '\n';
+         }
+
+          myfile.close();
+     }
+     else std::cout << "Unable to open file";
+     return 0;
 }
